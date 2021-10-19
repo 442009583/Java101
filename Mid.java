c5f13a69da3c2eb3a317eda1d9dd43ba0b3d19ca
442009583 <30395855+442009583@users.noreply.github.com>
2021-10-19 10:27:14 +0300

    Create Mid.java

---
+++ Mid.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Mid
+{
+	public static void main(String[] args) {
+	    String Name;
+	    int age;
+	    Scanner input = new Scanner(System.in);
+	    
+	    System.out.print("Name: ");
+	    Name = input.nextLine();
+	    
+	    System.out.print("Age: ");
+	    age = input.nextInt();
+	    
+	    int Dob = 2021-age;
+	    
+	    if (Dob < 1990)
+	        System.out.println("Hi "+Name+" in "+Dob+" you should have played Pac-man");
+	    
+	    else if (Dob > 1990 && Dob <2000)
+	        System.out.println("Hi "+Name+" in "+Dob+" you should have played Super Mario or Kick off 2");
+	    
+	    else
+	        System.out.println("Hi "+Name+" in "+Dob+" you should have played Fifa");
+	}
+}
