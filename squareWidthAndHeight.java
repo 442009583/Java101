12b684109b0ad1089b80cbb3c160c3fbe0401fb7
442009583 <30395855+442009583@users.noreply.github.com>
2021-12-14 01:46:20 +0300

    Create squareWidthAndHeight.java

---
+++ squareWidthAndHeight.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class squareWidthAndHeight
+{
+	public static void main(String[] args) {
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Width: ");
+	    int width = scan.nextInt();
+	    System.out.println("Height: ");
+	    int height = scan.nextInt();
+	    System.out.println(width * height);
+        }
+	}
+}
